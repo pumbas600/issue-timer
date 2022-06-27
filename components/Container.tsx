676d2dd30d48ee6e6e87f62470d6a4cf907adc44
pumbas600 <joshjeffers600@gmail.com>
2022-06-27 21:02:14 +1200

    Added utility Component type and Container

---
+++ components/Container.tsx
@@ -0,0 +1,7 @@
+import { Component } from '../types/Utility';
+
+const Container: Component = (props) => {
+    return <div className="lg:mx-auto m-5 lg:w-[900px]">{props.children}</div>;
+};
+
+export default Container;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
+function at(url) {
+    return `${url}**/*.{js,ts,jsx,tsx}`;
+}
+
 module.exports = {
-    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    content: [at('./pages/'), at('./components/'), at('./types/')],
     theme: {
         extend: {},
     },
