45bc43559116deb1d0760a6009a9a285ab4869e4
pumbas600 <joshjeffers600@gmail.com>
2022-06-29 22:36:03 +1200

    Created Checkbox component

---
+++ components/checkbox/Checkbox.tsx
@@ -0,0 +1,33 @@
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+import { ClassName } from '../../types/Props';
+import { Component } from '../../types/Utility';
+
+interface Props extends ClassName {
+    checked?: boolean;
+    onClicked?: (checked: boolean) => void;
+    ring?: boolean;
+}
+
+const Checkbox: Component<Props> = (props) => {
+    const [checked, setChecked] = useState(props.checked ?? false);
+
+    return (
+        <div
+            className={`flex items-center justify-center m-1 w-5 h-5 rounded-sm ${
+                checked ? 'bg-blue-500 hover:bg-blue-600' : 'border border-gray-500 hover:border-gray-600'
+            } ${props.ring && checked ? 'ring-offset-1 ring-2 ring-blue-500 hover:ring-blue-600' : ''} ${
+                props.className ?? ''
+            }`}
+            onClick={() => {
+                if (props.onClicked) props.onClicked(!checked);
+                setChecked((checked) => !checked);
+            }}
+        >
+            {checked && <FontAwesomeIcon className="text-white" icon={faCheck} />}
+        </div>
+    );
+};
+
+export default Checkbox;
