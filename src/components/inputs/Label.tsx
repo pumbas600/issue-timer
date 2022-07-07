2416549c7bcdf5b21ee45bbe5ba2f12aeac1ca52
pumbas600 <joshjeffers600@gmail.com>
2022-07-07 16:10:05 +1200

    #1 Added filters for PRs and Issues

--- src/components/inputs/Label.tsx
+++ src/components/inputs/Label.tsx
@@ -1,7 +1,9 @@
+import { merge } from '../../styles/Styles';
+import { ClassName } from '../../types/Props';
 import { Component } from '../../types/Utility';
 import Stack from '../utility/Stack';
 
-interface Props {
+interface Props extends ClassName {
     label: string | JSX.Element;
     placement?: 'left' | 'right' | 'above' | 'below';
 }
@@ -11,9 +13,9 @@ const Label: Component<Props> = (props) => {
     const isAfter = props.placement === 'right' || props.placement === 'below';
 
     return (
-        <Stack orientation={isRow ? 'row' : 'col'} className="gap-x-2 w-full">
+        <Stack orientation={isRow ? 'row' : 'col'} className={merge(['gap-x-2 w-full', props.className])}>
             {!isAfter && props.label}
-            <div>{props.children}</div>
+            {props.children}
             {isAfter && props.label}
         </Stack>
     );
